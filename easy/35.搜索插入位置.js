6aa0969dee5fb46d9b92e01e4d009c8ffa163991
mango <18381060672@163.com>
2020-04-28 16:39:37 +0800

    easy: 35, 53, 58

---
+++ easy/35.搜索插入位置.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=javascript
+ *
+ * [35] 搜索插入位置
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    if (nums[0] > target) {
+        return 0;
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] >= target) {
+            return i;
+        }
+    }
+    return nums.length;
+};
+// @lc code=end
+
